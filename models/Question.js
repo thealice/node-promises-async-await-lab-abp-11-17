a8a8b79951098ee7de918e6dd8a2096713ab9c4d
Alice Freda <alicefreda@gmail.com>
2017-11-18 18:34:03 +0000

    automatically backed up by learn

--- models/Question.js
+++ models/Question.js
@@ -11,7 +11,7 @@ class Question {
       setTimeout(function(){
         const questions = Question._All
         console.log("...Questions Loaded")
-
+        
         resolve(questions)
       }, 1500)
     })
@@ -29,14 +29,14 @@ class Question {
   // FIXME: Implement Promise
   static Find(id){
     console.log(`Waiting for Question ${id} to load...`)
-
+    return new Promise(function(resolve) {
       setTimeout(function(){
         const question = Question._All[id-1] // Do Not Edit
         console.log(`...Question ${id} Loaded`) // Do Not Edit
 
-        return question
+        resolve (question);
       }, 1500)
-
+    })
   }
 
   // FIXME: Implement async / await
